095faa2671189df01a082a644609618c28ebbe0d
PhoenixZ810 <z2855064151@gmail.com>
2023-11-17 16:53:22 +0800

    grit process

--- tools/dataset_converters/grit2odvg.py
+++ tools/dataset_converters/grit2odvg.py
@@ -42,21 +42,51 @@ def check_caption(cap):
     return True
 
 
+def check_box(box_list, w, h):
+    # import pdb;pdb.set_trace()
+    box_w = box_list[2] - box_list[0]
+    box_h = box_list[3] - box_list[1]
+    out_range = False
+    if not all(0 <= box_list[i] <= w - 1 if i % 2 == 0 else 0 <= box_list[i] <= h - 1 for i in range(4)):
+        # print('warning! box out of range')
+        box_list[0] = max(box_list[0], 0)
+        box_list[1] = max(box_list[1], 0)
+        box_list[2] = min(box_list[2], w - 1)
+        box_list[3] = min(box_list[3], h - 1)
+        out_range = True
+    if box_w < 1 or box_h < 1:
+        print('warning! box too small')
+        return False, out_range
+    else:
+        return True, out_range
+
+
 def get_regions(nc, anno):
     h = anno["height"]
     w = anno["width"]
+    out_ranges = 0
     if not isinstance(nc, list):
         phrase = clean_span(anno["caption"][int(nc[0][0]) : int(nc[0][1])])
+        index = [int(nc[0][0]), int(nc[0][1])]
         bbox = []
         for noun in nc:
             box = [round(noun[2] * w, 3), round(noun[3] * h, 3), round(noun[4] * w, 3), round(noun[5] * h, 3)]
-            bbox.append(box)
+            box_small, out_range = check_box(box, w, h)
+            out_ranges += 1 if out_range == True else 0
+            if not box_small:
+                return None
+            else:
+                bbox.append(box)
     else:
         phrase = clean_span(anno["caption"][int(nc[0]) : int(nc[1])])
+        index = [int(nc[0]), int(nc[1])]
         bbox = [round(nc[2] * w, 3), round(nc[3] * h, 3), round(nc[4] * w, 3), round(nc[5] * h, 3)]
-        if not all(0 <= bbox[i] <= w-1 if i % 2 == 0 else 0 <= bbox[i] <= h-1 for i in range(4)):
+        box_small, out_range = check_box(bbox, w, h)
+        out_ranges += 1 if out_range == True else 0
+        if not box_small:
             return None
-    return {"bbox": bbox, "phrase": phrase}
+
+    return {"phrase": phrase, "index": index, "bbox": bbox}, out_ranges
 
 
 # '''从所有图片中选择random_sampled张图片'''
@@ -74,15 +104,27 @@ def get_regions(nc, anno):
 
 def process_json(js, args):
     odvg = []
+    caption_false = 0
+    box_small = 0
+    out_ranges = 0
     if os.path.isdir(args.input_files):
         js = os.path.join(args.input_files, js)
-
     with open(js, "r") as f:
         metas = json.load(f)
         metas_len = len(metas)
+
     for meta in metas:
-        result = process_item(meta, args)
-        odvg.append(result)
+        result, out_range_per_image = process_item(meta, args)
+        if result is not None:
+            if len(result['grounding']['regions']) != 0:
+                odvg.append(result)
+            else:
+                box_small += 1
+            out_ranges += out_range_per_image
+        else:
+            caption_false += 1
+    print(f"images num in {js} = {metas_len}, after filter: {len(odvg)}, num of existing boxes out of range: {out_ranges}")
+    print(f"caption false: {caption_false}, box too small: {box_small}")
     odvg = list(filter(None, odvg))
     return odvg
 
@@ -99,11 +141,12 @@ def process_item(file, args):
     annotation = file
     # 若caption有除了ASCII文本以外的其他元素（.*/-emoji[CLS]等)，返回None
     if not check_caption(annotation):
-        return None
+        return None, None
     noun_chunks = annotation['noun_chunks']
     ref_exps = annotation['ref_exps']
     regions = []
     random_num = random.random()
+    out_ranges = 0
 
     # if annotation['clip_similarity_vitb32'] < 0.3:
     #     return None
@@ -132,10 +175,11 @@ def process_item(file, args):
                 elif num_repeat == 1:
                     repeat_noun.append(anno)
     for nc in repeat_noun:
-        region = get_regions(nc, annotation)
+        region, out_range = get_regions(nc, annotation)
         if region is not None:
             if str.isascii(region["phrase"]):
                 regions.append(region)
+            out_ranges += out_range
     # for re in ref_exps:
     #     region = get_regions(re, anno)
     #     if str.isascii(region["phrase"]):
@@ -149,7 +193,7 @@ def process_item(file, args):
         "width": annotation["width"],
         "grounding": {"caption": clean_span(annotation["caption"]), "regions": regions},
     }
-    return odvg_anno
+    return odvg_anno, out_ranges
 
 
 if __name__ == "__main__":
@@ -158,6 +202,7 @@ if __name__ == "__main__":
     # output_name = "./girt_14m_odvg.jsonl"
     parser = argparse.ArgumentParser(description="GRIT2ODVG List.")
     parser.add_argument("input_files", type=str)  # jsons directory
+    parser.add_argument("json_name", type=str)
     parser.add_argument("--root", type=str, default="", help="Source image root")
     parser.add_argument("--output_dir", type=str, default="./")
     parser.add_argument("--random_samples", type=int, default=2000)
@@ -170,18 +215,18 @@ if __name__ == "__main__":
     # # random sample
     # metas, metas_len = prepare_list(args.input_file, args.random_samples)
 
-    # # process single json file
-    # odvg_anno = process_json(args.input_files, args)
+    # process single json file
+    odvg_anno = process_json(args.input_files, args)
 
     # process jsons directory
-    odvg_anno = []
-    jsons = [i for i in os.listdir(args.input_files) if i.endswith('.json')]
-    func = partial(process_json, args=args)
-    with Pool(processes=args.process_num) as pool:
-        for result in tqdm(pool.imap(func=func, iterable=jsons), total=len(jsons)):
-            odvg_anno.extend(result)
+    # odvg_anno = []
+    # jsons = [i for i in os.listdir(args.input_files) if i.endswith('.json')]
+    # func = partial(process_json, args=args)
+    # with Pool(processes=args.process_num) as pool:
+    #     for result in tqdm(pool.imap(func=func, iterable=jsons), total=len(jsons)):
+    #         odvg_anno.extend(result)
 
-    print(f"after filter: {len(odvg_anno)}")
-    # json_name = os.path.join(args.output_dir, f'girt_14m_odvg_all.json')
-    # with jsonlines.open(json_name, mode="w") as fwriter:
-    #     fwriter.write_all(odvg_anno)
+    # print(f"after filter: {len(odvg_anno)}")
+    json_name = os.path.join(args.output_dir, args.json_name)
+    with jsonlines.open(json_name, mode="w") as fwriter:
+        fwriter.write_all(odvg_anno)
